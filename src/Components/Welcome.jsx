ba15c7e0f164d82eb60aaad04c54baf1f923a276
yash-patel04 <yashpate04@gmail.com>
2025-02-20 09:55:26 +0530

    carousel

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -1,9 +1,14 @@
+/* eslint-disable react/prop-types */
 import "../CSS/Welcome.css";
 import photo1 from "../assets/Images/1.jpg";
 import photo2 from "../assets/Images/2.jpg";
 import photo3 from "../assets/Images/3.jpg";
 import photo4 from "../assets/Images/4.jpg";
 import photo5 from "../assets/Images/5.jpg";
+
+const imgs = [photo1, photo2, photo3, photo4, photo5];
+
+import Carousel from "./Carousel";
 
 const Welcome = () => {
   return (
@@ -21,7 +26,7 @@ const Welcome = () => {
             </div>
           </h1>
         </div>
-        <div className="inner-container">
+        {/* <div className="inner-container">
           <div className="card">
             <img src={photo1} />
           </div>
@@ -37,7 +42,9 @@ const Welcome = () => {
           <div className="card">
             <img src={photo5} />
           </div>
-        </div>
+        </div> */}
+
+          <Carousel slides={imgs} />
       </div>
     </>
   );
