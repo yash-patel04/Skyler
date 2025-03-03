e7ae1dfe776d4d602284bf5beb0d277a5dba0f30
yash-patel04 <yashpate04@gmail.com>
2025-03-03 08:42:01 +0530

    photos

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -3,9 +3,8 @@ import photo1 from "../assets/Images/1.jpg";
 import photo2 from "../assets/Images/2.jpg";
 import photo3 from "../assets/Images/3.jpg";
 import photo4 from "../assets/Images/4.jpg";
-import photo5 from "../assets/Images/5.jpg";
 
-const imgs = [photo1, photo2, photo3, photo4, photo5];
+const imgs = [photo1, photo2, photo3, photo4];
 
 import Carousel from "./Carousel";
 
